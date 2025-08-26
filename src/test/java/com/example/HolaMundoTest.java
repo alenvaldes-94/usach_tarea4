98e623a2e9db112e9106a9a938ecc90345948c85
Alen <alienx77789@gmail.com>
2025-08-25 20:12:46 -0400

    correccion de errores. Console Output jenkins

--- src/test/java/com/example/HolaMundoTest.java
+++ src/test/java/com/example/HolaMundoTest.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HolaMundoTest {
+
     @Test
     void metodo1_alen() {
-        assertEquals("Hola mundo1", App.holaMundo("Alen"));
+        assertEquals("Hola Alen!", App.holaMundo("Alen"));
     }
 
     @Test
     void metodo2_alen() {
-        assertEquals("Hola mundo2", App.holaMundo(null));
-        assertEquals("Hola mundo2", App.holaMundo(""));
-        assertEquals("Hola mundo2", App.holaMundo("  "));
+        assertEquals("Hola mundo!", App.holaMundo(null));
+        assertEquals("Hola mundo!", App.holaMundo(""));
+        assertEquals("Hola mundo!", App.holaMundo("  "));
     }
 }
